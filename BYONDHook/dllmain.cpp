4278d4890f79dfa074ec1b24a018e8d4a5c7ee45
Lyut <1999ludo@live.it>
2021-12-10 01:50:35 +0100

    small additions

--- BYONDHook/dllmain.cpp
+++ BYONDHook/dllmain.cpp
@@ -3,6 +3,84 @@
 
 extern "C" { __declspec(dllimport) void Main(); }
 
+void __fastcall hkInitClient(void* This, void* _EDX)
+{
+    printf("[!] DungClient::InitClient called, stealing class instance...\n");
+    DungClient = This;
+    return oInitClient(This);
+}
+
+void __fastcall hkToHtml(void* This, void* _EDX, const char* a2)
+{
+    if (a2[21] == char(76) && a2[31] == char(46)) {
+        printf("[BYOND] Logging in...\n");
+    }
+    else if (a2[21] == char(99) && a2[29] == char(100)) {
+        printf("[BYOND] connected\n");
+    }
+    else if (a2[21] == char(76) && a2[32] == char(115)) {
+        std::string strTmp = a2;
+        try {
+            strTmp = strTmp.substr(21);
+        }
+        catch (std::out_of_range&) {
+            return;
+        }
+        size_t pos = strTmp.find("</span>");
+        if (pos != std::string::npos)
+        {
+            strTmp.erase(pos-1, std::string("</span>").length()+1);
+        }
+        printf("[BYOND] %s\n", strTmp);
+    }
+    else if (a2[21] == char(67) && a2[30] == char(103)) {
+        std::string strTmp = a2;
+        try {
+            strTmp = strTmp.substr(21);
+        }
+        catch (std::out_of_range&) {
+            return;
+        }
+        size_t pos = strTmp.find("</span>");
+        if (pos != std::string::npos)
+        {
+            strTmp.erase(pos, std::string("</span>").length());
+        }
+        printf("[BYOND] %s\n", strTmp);
+    }
+        return oToHtml(This, a2);
+}
+
+void __fastcall hkCommandEvent(void* This, void* _EDX, unsigned char a, unsigned short b, unsigned char c)
+{
+    DWORD* dwCommandPtr;
+    DWORD dwCommandLength;
+
+    __asm
+    {
+        push eax
+        mov eax, esi
+        mov dwCommandPtr, eax
+        mov eax, edx
+        mov dwCommandLength, eax
+        pop eax
+    }
+
+    if (dwCommandLength > 1000)
+        return oCommandEvent(This, a, b, c);
+
+    char* commandString = new char[dwCommandLength - 1];
+    commandString = (char*)dwCommandPtr;
+
+    if (true)
+    {
+        DWORD lastError = GetLastError();
+        std::cout << "((byond::commandevent))" << commandString << std::endl;
+    }
+
+    return oCommandEvent(This, a, b, c);
+}
+
 int __stdcall IsByondMember_hk(int* a1)
 {
     return 1;
@@ -20,13 +98,13 @@ BOOL __stdcall GVIA_hk(LPCSTR a1, LPSTR a2, DWORD a3, LPDWORD a4, LPDWORD a5, LP
 {    
     UINT spoofedVolume = GetPrivateProfileIntA("CID", "volume", 7776, "C:\\BYOND\\cid.ini");
     printf("[!] GVIA called! Spoofing CID to %i...\n", spoofedVolume);
-    printf("[!] Logging in as %s...\n", GetCurrentLoginKey());
+    //printf("[!] Logging in as %s...\n", GetCurrentLoginKey());
     *(DWORD*)a4 = spoofedVolume;
 
     return TRUE;
 }
 
-void* __stdcall ComputeCid_hk(char* a1, size_t a2)
+/*void* __stdcall ComputeCid_hk(char* a1, size_t a2)
 {
     printf("[!] ComputeCid called: %s\n", a1);
 
@@ -53,7 +131,7 @@ void* __stdcall ComputeCid_hk(char* a1, size_t a2)
     VirtualProtect(pCIDMD5, 32, OldProtection, &OldProtection);
     VirtualProtect(pCIDMD5unk, 32, OldProtection, &OldProtection);
     printf("[+] Protection changed back to original\n");*/
-}
+//} 
 
 void Main() {
     AllocConsole();
@@ -78,28 +156,52 @@ void Main() {
     {
         std::cout << "Failed to hook DungClient::IsByondMember..." << std::endl;
     }
-
     printf("[+] IsByondMember hooked, address: 0x%p\n", GetProcAddress(ByondCore, DungClientIsByondMember));
+
+    if (MH_CreateHook(GetProcAddress(ByondCore, DungClientInitClient), hkInitClient, reinterpret_cast<LPVOID*>(&oInitClient)) != MH_OK)
+    {
+        std::cout << "Failed to hook DungClient::InitClient..." << std::endl;
+        return;
+    }
+    printf("[+] InitClient hooked, address: 0x%p\n", GetProcAddress(ByondCore, DungClientInitClient));
+
+    if (MH_CreateHook(GetProcAddress(ByondCore, DMTextPrinterToHtml), hkToHtml, reinterpret_cast<LPVOID*>(&oToHtml)) != MH_OK)
+    {
+        std::cout << "Failed to hook DMTextPrinter::ToHtml..." << std::endl;
+        return;
+    }
+    printf("[+] ToHtml hooked, address: 0x%p\n", GetProcAddress(ByondCore, DMTextPrinterToHtml));
+
+    if (MH_CreateHook(GetProcAddress(ByondCore, DungClientCommandEvent), hkCommandEvent, reinterpret_cast<LPVOID*>(&oCommandEvent)) != MH_OK)
+    {
+        std::cout << "Failed to hook DungClient::CommandEvent..." << std::endl;
+    }
+    printf("[+] CommandEvent hooked, address: 0x%p\n", GetProcAddress(ByondCore, DungClientCommandEvent));
 
     if (MH_CreateHookApiEx(L"kernel32.dll", "GetVolumeInformationA", GVIA_hk, (LPVOID*)&GVIA_o, NULL) != MH_OK)
     {
         std::cout << "Failed to hook Kernel32::GetVolumeInformationA..." << std::endl;
     }
-
     printf("[+] GetVolumeInformationA function hooked, address: 0x%p\n", GVIA_o);
 
     if (MH_CreateHookApiEx(L"kernel32.dll", "GetVersionExW", GVEW_hk, (LPVOID*)&GVEW_o, NULL) != MH_OK)
     {
         std::cout << "Failed to hook Kernel32::GetVersionExW..." << std::endl;
     }
-
     printf("[+] GetVersionExW function hooked, address: 0x%p\n", GVEW_o);
 
     if (MH_EnableHook(MH_ALL_HOOKS) != MH_OK)
     {
         std::cout << "Fatal Error!" << std::endl;
     }
+
 }
+
+//  if (MH_CreateHook(pComputeCid, &ComputeCid_hk, reinterpret_cast<LPVOID*>(&ComputeCid_o)) != MH_OK)
+//  {
+ //     std::cout << "Failed to hook ComputeCID function..." << std::endl;
+ // }
+  //printf("[+] ComputeCid function hooked, address: 0x%p\n", pComputeCid);
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
--- BYONDHook/includes.h
+++ BYONDHook/includes.h
@@ -14,10 +14,14 @@ Name								Address		Ordinal
 DungClient::IsByondMember(long *)	10078AA0	1652
 DungPager::IsByondMember(long *)	1024F270	1653
 */
+
 #define DMTextPrinterBeep "?Beep@DMTextPrinter@@UAEXXZ"
+#define DMTextPrinterToHtml "?ToHtml@DMTextPrinter@@QAEPADPBD@Z"
 #define DungClientIsByondMember "?IsByondMember@DungClient@@QAE_NPAJ@Z"
+#define DungClientInitClient "?InitClient@DungClient@@QAEXXZ"
 #define DungClientGetCurrentLoginKey "?GetCurrentLoginKey@DungClient@@QAEPBDXZ"
 #define DungClientGetServerIP "?GetServerIP@DungClient@@QAEPBDPBD@Z"
+#define DungClientCommandEvent "?CommandEvent@DungClient@@QAEXEGE@Z"
 #define ByondLibGetByondVersion "?GetByondVersion@ByondLib@@QAEJXZ"
 #define ByondLibGetByondBuild "?GetByondBuild@ByondLib@@QAEJXZ"
 #define ByondLibGetByondLabel "?GetByondLabel@ByondLib@@QAEPBDXZ"
@@ -27,9 +31,9 @@ DungPager::IsByondMember(long *)	1024F270	1653
 HMODULE ByondCore = GetModuleHandleA("byondcore.dll");
 HMODULE Kernel32Handle = GetModuleHandleA("kernel32.dll");
 
-typedef DWORD(__thiscall* GetByondVersionFn)(void);
+typedef DWORD (__thiscall* GetByondVersionFn)(void);
 GetByondVersionFn GetByondVersion = (GetByondVersionFn)GetProcAddress(ByondCore, ByondLibGetByondVersion);
-typedef DWORD(__thiscall* GetByondBuildFn)(void);
+typedef DWORD (__thiscall* GetByondBuildFn)(void);
 GetByondBuildFn GetByondBuild = (GetByondBuildFn)GetProcAddress(ByondCore, ByondLibGetByondBuild);
 typedef const char* (__thiscall* GetByondLabelFn)(void);
 GetByondLabelFn GetByondLabel = (GetByondLabelFn)GetProcAddress(ByondCore, ByondLibGetByondLabel);
@@ -42,17 +46,33 @@ GetServerIPFn GetServerIP = (GetServerIPFn)GetProcAddress(ByondCore, DungClientG
 typedef const char* (__thiscall* GetByondHubPathFn)(void);
 GetByondHubPathFn GetByondHubPath = (GetByondHubPathFn)GetProcAddress(ByondCore, ByondLibGetByondHubPath);
 
+//typedef unsigned __int8 (__thiscall* IsDebuggingNetworkFn)(void*);
+//IsDebuggingNetworkFn IsDebuggingNetwork = (IsDebuggingNetworkFn)GetProcAddress(ByondCore, "?IsDebuggingNetwork@DungClient@@QAEEXZ");
+
+typedef void(__thiscall* BeepFn)(void*);
+BeepFn Beep_ = (BeepFn)GetProcAddress(ByondCore, DMTextPrinterBeep);
+
 typedef void(__stdcall* ComputeCid_t)(char* a1, size_t a2);
 typedef void(__stdcall* GVEW_t)(LPOSVERSIONINFOW a1);
-typedef void(__stdcall* DMTextPrinter_t)(void* _this);
 typedef int(__stdcall* GVIA_t)(LPCSTR a1, LPSTR a2, DWORD a3, LPDWORD a4, LPDWORD a5, LPDWORD a6, LPSTR a7, DWORD a8);
 GVIA_t GVIA_o = NULL;
 GVEW_t GVEW_o = NULL;
-DMTextPrinter_t DMTextPrinter_o = NULL;
 ComputeCid_t ComputeCid_o = NULL;
+
+typedef void(__thiscall* InitClient)(void*);
+static InitClient oInitClient;
+void __fastcall hkInitClient(void* This, void* _EDX);
+
+typedef void(__thiscall* ToHtml)(void*, const char* a2);
+static ToHtml oToHtml;
+void __fastcall hkToHtml(void* This, void* _EDX, const char* a2);
+
+typedef void(__thiscall* CommandEvent)(void*, unsigned char, unsigned short, unsigned char);
+static CommandEvent oCommandEvent;
+void __fastcall hkCommandEvent(void* This, void* _EDX, unsigned char a, unsigned short b, unsigned char c);
 
 PBYTE pCIDMD5 = (PBYTE)ByondCore + 0x373660;
 PBYTE pCIDMD5unk = (PBYTE)ByondCore + 0x372E6C;
 PBYTE pComputeCid = (PBYTE)ByondCore + 0x228D40;
 PBYTE pUnkFunc = (PBYTE)ByondCore + 0x229AB0;
-
+void* DungClient;
